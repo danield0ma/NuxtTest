683495df205412fa718b861c3487d2073ff820a0
daniel.d0ma <daniel.d0ma@protonmail.com>
2022-11-16 15:47:49 +0100

    api comm

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Index</h1>
+  <h1>Index {{ this.text }}</h1>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  mounted() {
+    this.getText()
+  },
+  methods: {
+    async getText() {
+      let msg = await fetch('https://localhost:7032/API/Test/Testing/')
+      const data = await msg.json()
+      console.log(data)
+      console.log(data.message)
+      this.text = data.message
+    }
+  }
 }
 </script>
